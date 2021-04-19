34fecc0a3c9159937ec2f27d583a4ae27290ba55
rfick <rfick@ufl.edu>
2021-04-19 14:11:45 -0400

    compare data to historical average

--- calc_meanandvar.py
+++ calc_meanandvar.py
@@ -6,18 +6,28 @@ import cv2
 import math
 import os
 
-# Computes estimated chlorophyll-a concentration according to the CI algorithm
-# See: https://oceancolor.gsfc.nasa.gov/atbd/chlor_a/
-def OCI(Rrs_red, Rrs_green, Rrs_blue, wv_red, wv_green, wv_blue):
-	return Rrs_green - [Rrs_blue + ((wv_green - wv_blue)/(wv_red - wv_blue))*(Rrs_red - Rrs_blue)]
+# For a new value newValue, compute the new count, new mean, the new M2.
+# mean accumulates the mean of the entire dataset
+# M2 aggregates the squared distance from the mean
+# count aggregates the number of samples seen so far
+def update(count, mean, M2, newValue):
+	count += 1
+	delta = newValue - mean
+	mean += delta / count
+	delta2 = newValue - mean
+	M2 += delta * delta2
+	return count, mean, M2
 
-# Computes estimated chlorophyll-a concentration according to the OCx algorithm
-# See: https://oceancolor.gsfc.nasa.gov/atbd/chlor_a/
-def OCX(Rrs_blue, Rrs_green, a0, a1, a2, a3, a4):
-	return np.power(10, a0 + a1*(np.log10(Rrs_blue/Rrs_green)) + a2*np.power(np.log10(Rrs_blue/Rrs_green), 2) + a3*np.power(np.log10(Rrs_blue/Rrs_green), 3) + a4*np.power(np.log10(Rrs_blue/Rrs_green), 4))
+# Retrieve the mean, variance and sample variance from an aggregate
+def finalize(count, mean, M2):
+	if count < 2:
+		return float("nan"), float("nan")
+	else:
+		(mean, variance) = (mean, M2 / count)
+		return mean, variance
 
 data_folder = 'landsat8_data'
-data_list = ['LC08_L2SP_016042_20210112_20210308_02_T2']
+data_list = os.listdir(data_folder)
 
 for i in range(len(data_list)):
 	file_id = data_list[i]
@@ -31,8 +41,6 @@ for i in range(len(data_list)):
 
 	# Cloud bit is in position 5
 	water_mask = (im_QA >> 4) & 0b11111
-
-	water_mask_hold = water_mask
 
 	water_mask = water_mask == 28
 	water_mask = water_mask.astype(np.uint8)
@@ -65,8 +73,6 @@ for i in range(len(data_list)):
 		if('CORNER_LR_LON_PRODUCT' in line):
 			CORNER_LR_LON_PRODUCT = float(line.split("= ",1)[1])
 	metadatafile.close()
-
-	#print(str(CORNER_UL_LAT_PRODUCT) + ' ' + str(CORNER_UL_LON_PRODUCT) + ' ' + str(CORNER_LR_LAT_PRODUCT) + ' ' + str(CORNER_LR_LON_PRODUCT))
 
 	# Lat + long of bounding box of the study area (NW corner and SE corner)
 	latNW = 26.984196
@@ -101,90 +107,43 @@ for i in range(len(data_list)):
 	if(lonPixelDiffSE > im_rgb.shape[1]-1):
 		lonPixelDiffSE = im_rgb.shape[1]-1
 
-	#plt.figure()
-	#plt.imshow(water_mask_hold[latPixelDiffNW:latPixelDiffSE, lonPixelDiffNW:lonPixelDiffSE])
-	#plt.colorbar()
-
-	# Characteristics of Landsat 8
-	# See: https://en.wikipedia.org/wiki/Landsat_8
-	wv_blue = 443
-	wv_green = 562.5
-	wv_red = 655
-
-	# Pulling some coefficients for the OCTS sensor from https://oceancolor.gsfc.nasa.gov/atbd/chlor_a/
-	a0 = 0.2412
-	a1 = -2.0546
-	a2 = 1.1776
-	a3 = -0.5538
-	a4 = -0.4570
-
-	#nonzero_blue = np.zeros_like(im_blue)
-	#nonzero_blue[im_blue==0] = 1
-
-	#plt.figure()
-	#plt.imshow(nonzero_blue)
-	#plt.show()
-	#asldkj
-
-	#blue_flatten = im_blue.flatten()
-	#plt.figure()
-	#plt.hist(blue_flatten, bins=100)
-	#green_flatten = im_green.flatten()
-	#plt.figure()
-	#plt.hist(green_flatten, bins=100)
-	#red_flatten = im_red.flatten()
-	#plt.figure()
-	#plt.hist(red_flatten, bins=100)
-	#plt.show()
-
 	# Scale values from 1-65535 to 0-1
 	im_blue = im_blue/65535
 	im_green = im_green/65535
 	im_red = im_red/65535
 
-	#CI = OCI(im_red, im_green, im_blue, wv_red, wv_green, wv_blue)
-	#CI = np.squeeze(CI)
-
-	#CX = OCX(im_blue, im_green, a0, a1, a2, a3, a4)
-
 	im_rgb[:, :, 0] = im_red
 	im_rgb[:, :, 1] = im_green
 	im_rgb[:, :, 2] = im_blue
 
-	im_rgb = np.sqrt(im_rgb)
-
-	plt.figure()
-	plt.imshow(im_rgb)
-	plt.title('Full Scene')
-	plt.savefig('full_scene.png', bbox_inches='tight')
-
-	plt.figure()
-	plt.imshow(im_rgb[latPixelDiffNW:latPixelDiffSE, lonPixelDiffNW:lonPixelDiffSE, :])
-	plt.title('Study Area')
-	plt.savefig('study_area.png', bbox_inches='tight')
-
 	water_rgb = np.zeros_like(im_rgb)
 	cv2.bitwise_and(im_rgb, im_rgb, water_rgb, water_mask)
 
-	plt.figure()
-	plt.imshow(water_mask_hold[latPixelDiffNW:latPixelDiffSE, lonPixelDiffNW:lonPixelDiffSE])
-	plt.title('Water Mask')
-	plt.savefig('water_mask.png', bbox_inches='tight')
+	study_area = water_rgb[latPixelDiffNW:latPixelDiffSE, lonPixelDiffNW:lonPixelDiffSE, :]
 
-	plt.figure()
-	plt.imshow(water_rgb[latPixelDiffNW:latPixelDiffSE, lonPixelDiffNW:lonPixelDiffSE, :])
-	plt.title('Study Area - Water')
-	plt.savefig('study_water.png', bbox_inches='tight')
+	print('file {}'.format(i))
 
-	#plt.figure()
-	#plt.imshow(water_mask[latPixelDiffNW:latPixelDiffSE, lonPixelDiffNW:lonPixelDiffSE])
-	#plt.colorbar()
+	# First time through, initialize empty arrays
+	if(i==0):
+		#Keeps running count, mean, and M2 for B, G, and R bands
+		runningAggregates = np.zeros((study_area.shape[0], study_area.shape[1], 9))
+		for j in range(runningAggregates.shape[0]):
+			for k in range(runningAggregates.shape[1]):
+				if(study_area[j, k, 0]!=0 or study_area[j, k, 1]!=0 or study_area[j, k, 2]!=0):
+					for band in range(3):
+						runningAggregates[j, k, 3*band], runningAggregates[j, k, 3*band+1], runningAggregates[j, k, 3*band+2] = update(runningAggregates[j, k, 3*band], runningAggregates[j, k, 3*band+1], runningAggregates[j, k, 3*band+2], study_area[j, k, band])
+	else:
+		for j in range(study_area.shape[0]):
+			for k in range(study_area.shape[1]):
+				if((study_area[j, k, 0]!=0 or study_area[j, k, 1]!=0 or study_area[j, k, 2]!=0) and j<runningAggregates.shape[0] and k<runningAggregates.shape[1]):
+					for band in range(3):
+						runningAggregates[j, k, 3*band], runningAggregates[j, k, 3*band+1], runningAggregates[j, k, 3*band+2] = update(runningAggregates[j, k, 3*band], runningAggregates[j, k, 3*band+1], runningAggregates[j, k, 3*band+2], study_area[j, k, band])
 
-	#plt.figure()
-	#plt.imshow(CI[latPixelDiffNW:latPixelDiffSE, lonPixelDiffNW:lonPixelDiffSE])
-	#plt.colorbar()
+meanAndVar = np.zeros((study_area.shape[0], study_area.shape[1], 6))
 
-	#plt.figure()
-	#plt.imshow(CX[latPixelDiffNW:latPixelDiffSE, lonPixelDiffNW:lonPixelDiffSE])
-	#plt.clim(0, 10)
-	#plt.colorbar()
+for j in range(runningAggregates.shape[0]):
+	for k in range(runningAggregates.shape[1]):
+		for band in range(3):
+			meanAndVar[j, k, 2*band], meanAndVar[j, k, 2*band+1] = finalize(runningAggregates[j, k, 3*band], runningAggregates[j, k, 3*band+1], runningAggregates[j, k, 3*band+2])
+
+np.save('meanAndVar.npy', meanAndVar)
--- plotAnomalies.py
+++ plotAnomalies.py
@@ -1,23 +1,17 @@
-from PIL import Image
 import numpy as np
 import matplotlib.pyplot as plt
 from skimage import io
-import cv2
-import math
 import os
+import cv2
 
-# Computes estimated chlorophyll-a concentration according to the CI algorithm
-# See: https://oceancolor.gsfc.nasa.gov/atbd/chlor_a/
-def OCI(Rrs_red, Rrs_green, Rrs_blue, wv_red, wv_green, wv_blue):
-	return Rrs_green - [Rrs_blue + ((wv_green - wv_blue)/(wv_red - wv_blue))*(Rrs_red - Rrs_blue)]
+def ensure_folder(folder):
+	if not os.path.isdir(folder):
+		os.mkdir(folder)
 
-# Computes estimated chlorophyll-a concentration according to the OCx algorithm
-# See: https://oceancolor.gsfc.nasa.gov/atbd/chlor_a/
-def OCX(Rrs_blue, Rrs_green, a0, a1, a2, a3, a4):
-	return np.power(10, a0 + a1*(np.log10(Rrs_blue/Rrs_green)) + a2*np.power(np.log10(Rrs_blue/Rrs_green), 2) + a3*np.power(np.log10(Rrs_blue/Rrs_green), 3) + a4*np.power(np.log10(Rrs_blue/Rrs_green), 4))
+meanAndVar = np.load('meanAndVar.npy')
 
 data_folder = 'landsat8_data'
-data_list = ['LC08_L2SP_016042_20210112_20210308_02_T2']
+data_list = os.listdir(data_folder)
 
 for i in range(len(data_list)):
 	file_id = data_list[i]
@@ -31,8 +25,6 @@ for i in range(len(data_list)):
 
 	# Cloud bit is in position 5
 	water_mask = (im_QA >> 4) & 0b11111
-
-	water_mask_hold = water_mask
 
 	water_mask = water_mask == 28
 	water_mask = water_mask.astype(np.uint8)
@@ -65,8 +57,6 @@ for i in range(len(data_list)):
 		if('CORNER_LR_LON_PRODUCT' in line):
 			CORNER_LR_LON_PRODUCT = float(line.split("= ",1)[1])
 	metadatafile.close()
-
-	#print(str(CORNER_UL_LAT_PRODUCT) + ' ' + str(CORNER_UL_LON_PRODUCT) + ' ' + str(CORNER_LR_LAT_PRODUCT) + ' ' + str(CORNER_LR_LON_PRODUCT))
 
 	# Lat + long of bounding box of the study area (NW corner and SE corner)
 	latNW = 26.984196
@@ -101,90 +91,37 @@ for i in range(len(data_list)):
 	if(lonPixelDiffSE > im_rgb.shape[1]-1):
 		lonPixelDiffSE = im_rgb.shape[1]-1
 
-	#plt.figure()
-	#plt.imshow(water_mask_hold[latPixelDiffNW:latPixelDiffSE, lonPixelDiffNW:lonPixelDiffSE])
-	#plt.colorbar()
-
-	# Characteristics of Landsat 8
-	# See: https://en.wikipedia.org/wiki/Landsat_8
-	wv_blue = 443
-	wv_green = 562.5
-	wv_red = 655
-
-	# Pulling some coefficients for the OCTS sensor from https://oceancolor.gsfc.nasa.gov/atbd/chlor_a/
-	a0 = 0.2412
-	a1 = -2.0546
-	a2 = 1.1776
-	a3 = -0.5538
-	a4 = -0.4570
-
-	#nonzero_blue = np.zeros_like(im_blue)
-	#nonzero_blue[im_blue==0] = 1
-
-	#plt.figure()
-	#plt.imshow(nonzero_blue)
-	#plt.show()
-	#asldkj
-
-	#blue_flatten = im_blue.flatten()
-	#plt.figure()
-	#plt.hist(blue_flatten, bins=100)
-	#green_flatten = im_green.flatten()
-	#plt.figure()
-	#plt.hist(green_flatten, bins=100)
-	#red_flatten = im_red.flatten()
-	#plt.figure()
-	#plt.hist(red_flatten, bins=100)
-	#plt.show()
-
 	# Scale values from 1-65535 to 0-1
 	im_blue = im_blue/65535
 	im_green = im_green/65535
 	im_red = im_red/65535
 
-	#CI = OCI(im_red, im_green, im_blue, wv_red, wv_green, wv_blue)
-	#CI = np.squeeze(CI)
-
-	#CX = OCX(im_blue, im_green, a0, a1, a2, a3, a4)
-
 	im_rgb[:, :, 0] = im_red
 	im_rgb[:, :, 1] = im_green
 	im_rgb[:, :, 2] = im_blue
 
-	im_rgb = np.sqrt(im_rgb)
-
-	plt.figure()
-	plt.imshow(im_rgb)
-	plt.title('Full Scene')
-	plt.savefig('full_scene.png', bbox_inches='tight')
-
-	plt.figure()
-	plt.imshow(im_rgb[latPixelDiffNW:latPixelDiffSE, lonPixelDiffNW:lonPixelDiffSE, :])
-	plt.title('Study Area')
-	plt.savefig('study_area.png', bbox_inches='tight')
-
 	water_rgb = np.zeros_like(im_rgb)
 	cv2.bitwise_and(im_rgb, im_rgb, water_rgb, water_mask)
 
+	study_area = water_rgb[latPixelDiffNW:latPixelDiffSE, lonPixelDiffNW:lonPixelDiffSE, :]
+
+	print('file {}'.format(i))
+
+	anomalies = np.zeros_like(study_area)
+
+	for j in range(study_area.shape[0]):
+		for k in range(study_area.shape[1]):
+			if((study_area[j, k, 0]!=0 or study_area[j, k, 1]!=0 or study_area[j, k, 2]!=0) and j<meanAndVar.shape[0] and k<meanAndVar.shape[1]):
+				for band in range(3):
+					if(meanAndVar[j, k, 2*band+1]!=0):
+						anomalies[j, k, band] = abs((study_area[j, k, band] - meanAndVar[j, k, 2*band])/np.sqrt(meanAndVar[j, k, 2*band+1]))
+						anomalies[j, k, band] = anomalies[j, k, band]/5
+	np.clip(anomalies, 0, 1)
+
+	ensure_folder('outputAnomalies')
 	plt.figure()
-	plt.imshow(water_mask_hold[latPixelDiffNW:latPixelDiffSE, lonPixelDiffNW:lonPixelDiffSE])
-	plt.title('Water Mask')
-	plt.savefig('water_mask.png', bbox_inches='tight')
-
-	plt.figure()
-	plt.imshow(water_rgb[latPixelDiffNW:latPixelDiffSE, lonPixelDiffNW:lonPixelDiffSE, :])
-	plt.title('Study Area - Water')
-	plt.savefig('study_water.png', bbox_inches='tight')
-
-	#plt.figure()
-	#plt.imshow(water_mask[latPixelDiffNW:latPixelDiffSE, lonPixelDiffNW:lonPixelDiffSE])
-	#plt.colorbar()
-
-	#plt.figure()
-	#plt.imshow(CI[latPixelDiffNW:latPixelDiffSE, lonPixelDiffNW:lonPixelDiffSE])
-	#plt.colorbar()
-
-	#plt.figure()
-	#plt.imshow(CX[latPixelDiffNW:latPixelDiffSE, lonPixelDiffNW:lonPixelDiffSE])
-	#plt.clim(0, 10)
-	#plt.colorbar()
+	plt.imshow(anomalies)
+	plt.title(file_id)
+	writename = 'outputAnomalies/frame'+str(i).zfill(3)+'.jpg'
+	plt.savefig(writename)
+	plt.close()
